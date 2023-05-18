52b83445568eaee51d0c1593c56944e6d4190d26
minalraskar <131401868+minalraskar@users.noreply.github.com>
2023-05-18 18:15:26 +0530

    second commit

--- Modal.js
+++ Modal.js
@@ -1,36 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
- 
-    if (!localStorage.getItem("cookiePreference")) {
-      showCookiePopup();
-    }
-  
-    
-    function showCookiePopup() {
-      var cookiePopup = document.getElementById("cookie-popup");
-      cookiePopup.classList.remove("hidden");
-    }
-  
-  
-    function hideCookiePopup() {
-      var cookiePopup = document.getElementById("cookie-popup");
-      cookiePopup.classList.add("hidden");
-    }
-  
-  
+window.addEventListener("load", function () {
     function savePreference() {
-      var checkboxes = document.querySelectorAll("input[name='cookies']");
-      var selectedCookies = Array.from(checkboxes)
-        .filter(checkbox => checkbox.checked)
-        .map(checkbox => checkbox.value);
-  
-      localStorage.setItem("cookiePreference", selectedCookies.join(","));
-      hideCookiePopup();
-    }
-  
-   
-    var savePreferenceButton = document.getElementById("save-preference");
-    savePreferenceButton.addEventListener("click", savePreference);
-  
-   
-    setTimeout(hideCookiePopup, 10000);
-  });
+        var checkboxes = document.querySelectorAll("input[name='cookies']");
+        var selectedCookies = Array.from(checkboxes)
+          .filter(checkbox => checkbox.checked)
+          .map(checkbox => checkbox.value);
+    
+        localStorage.setItem("cookiePreference", selectedCookies.join(","));
+        hideCookiePopup();
+      }
+    
+     
+      var savePreferenceButton = document.getElementById("save-preference");
+      savePreferenceButton.addEventListener("click", savePreference);
+    
+     
+      
+    setTimeout(
+        function open(event) {
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+document.querySelector("#close").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "none";
+});
+
